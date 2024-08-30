18c953fa9d032d9550a8fe8358cebe1dc93f9e36
lucy2003fer <lucy48fernada@gmail.com>
2024-08-30 11:15:18 -0500

    Gestion de tareas

---
+++ gestion/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Tareas(models.Model):
+    estados = [('terminado','tarea completa'),
+               ('incompleto','tarea pendiente')]
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+    estado = models.CharField(max_length=100,choices=estados,default='incompleto')
+    def __str__(self):
+        return self.titulo
